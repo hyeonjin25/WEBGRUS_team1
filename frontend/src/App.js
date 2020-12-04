77322237cc5de1c9a54b78f3fe60261e6f0a572d
sohyeonjin <shj41738866@gmail.com>
2020-12-04 13:33:49 +0900

    implement authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./component/bar/Navbar";
-import Footer from "./component/Footer";
+import Footer from "./component/bar/Footer";
 import Main from "./component/Main";
 import LoginPage from "./component/LoginPage";
 import RegisterPage from "./component/RegisterPage";
 import Mypage from "./component/Mypage";
-import Newpost from "./component/fileUpload/Newpost";
+import Newpost from "./component/Newpost";
+import Auth from './hoc/auth'
 
 function App() {
   return (
@@ -17,11 +18,11 @@ function App() {
           style={{ minHeight: "calc(100vh-80px)", padding: "20px 0 20px 0" }}
         >
           <Switch>
-            <Route exact path='/' component={Main} />
-            <Route exact path='/login' component={LoginPage} />
-            <Route exact path='/register' component={RegisterPage} />
-            <Route exact path='/mypage' component={Mypage} />
-            <Route exact path='/newpost' component={Newpost} />
+            <Route exact path='/' component={Auth(Main,null)} />
+            <Route exact path='/login' component={Auth(LoginPage,false)} />
+            <Route exact path='/register' component={Auth(RegisterPage,false)} />
+            <Route exact path='/mypage' component={Auth(Mypage,true)} />
+            <Route exact path='/newpost' component={Auth(Newpost,true)} />
           </Switch>
         </div>
         <Footer />

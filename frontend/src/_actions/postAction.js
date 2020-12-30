8ddc9e39f707bf1a512b47f4de033a4342059a02
sohyeonjin <shj41738866@gmail.com>
2020-12-30 21:00:33 +0900

    update actions

--- frontend/src/_actions/postAction.js
+++ frontend/src/_actions/postAction.js
@@ -4,13 +4,26 @@ import { SERVER_API } from "./config";
 //action type
 export const UPLOAD_FILE = "upload_file";
 
-//액션 생성자 함수들
+// 액션 생성자 함수들
+
+//포스트 업로드
 export function fileUpload(data) {
   const request = axios
-    .post(`${SERVER_API}/post`, data)
+    .post(`${SERVER_API}/api/posts`, data)
     .then((res) => res.data);
   return {
     type: UPLOAD_FILE,
     payload: request,
   };
 }
+
+// //해당 유저의 포스트 정보받기
+// export function fileUpload(data) {
+//     const request = axios
+//       .post(`${SERVER_API}/api/posts`, data)
+//       .then((res) => res.data);
+//     return {
+//       type: UPLOAD_FILE,
+//       payload: request,
+//     };
+//   }

ffdd020de279053080b330f9c7cf10431695fc4c
sohyeonjin <shj41738866@gmail.com>
2021-01-11 21:27:46 +0900

    Rename getMyposts to getUserposts

--- frontend/src/routes/Mypage.js
+++ frontend/src/routes/Mypage.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { SERVER_API } from "../_actions/config";
 import Post from "../component/Post";
 import { useDispatch, useSelector } from "react-redux";
-import { getMyposts } from "../_actions/postAction";
+import { getUserposts } from "../_actions/postAction";
 
 function Mypage() {
   const [Posts, setPosts] = useState([]);
   const dispatch = useDispatch();
   const userid = useSelector((state) => state.user.userData.userid);
   useEffect(() => {
-    dispatch(getMyposts(userid)).then((res) => {
+    dispatch(getUserposts(userid)).then((res) => {
       setPosts(res.payload.posts);
     });
   }, []);

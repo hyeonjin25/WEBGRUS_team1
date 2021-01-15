96b33ea5dc10d5fd90f1627bf7492a04711470c3
sohyeonjin <shj41738866@gmail.com>
2021-01-15 19:51:57 +0900

    Fix error (Move bracket)

--- frontend/src/routes/Newpost.js
+++ frontend/src/routes/Newpost.js
@@ -108,146 +108,146 @@ function Newpost(props) {
 
       if (Description) formdata.append("description", Description);
       if (Tags.length > 0) {
-        Tags.forEach((tag) => {
-          formdata.append("tags", tag);
-        });
-
-        dispatch(fileUpload(formdata)).then((res) => {
-          //업로드 성공시 업로드된 페이지로 이동
-          if (res) {
-            props.history.push("/mypage");
-          } else {
-            alert("업로드에 실패했습니다.");
-          }
-        });
+        Tags.forEach((tag)=>{
+          formdata.append("tags", tag)
+        })
       }
+
+      dispatch(fileUpload(formdata)).then((res) => {
+        //업로드 성공시 업로드된 페이지로 이동
+        if (res) {
+          props.history.push("/mypage");
+        } else {
+          alert("업로드에 실패했습니다.");
+        }
+      });
     }
+  };
 
-    //input style
-    const textfield = {
-      margin: "5px 0 10px 0",
-      width: "40vw",
-      minWidth: "400px",
-    };
+  //input style
+  const textfield = {
+    margin: "5px 0 10px 0",
+    width: "40vw",
+    minWidth: "400px",
+  };
 
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <h1>UPLOAD</h1>
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>UPLOAD</h1>
+      <TextField
+        style={textfield}
+        required
+        id='standard-required'
+        label='제목'
+        variant='outlined'
+        onChange={onTitle}
+      />
+      <TextField
+        style={textfield}
+        id='standard-multiline-static'
+        label='작품소개'
+        multiline
+        rows={4}
+        variant='outlined'
+        onChange={onDescription}
+      />
+      {/* 태그입력창 */}
+      <div>
         <TextField
-          style={textfield}
-          required
-          id='standard-required'
-          label='제목'
-          variant='outlined'
-          onChange={onTitle}
-        />
-        <TextField
-          style={textfield}
           id='standard-multiline-static'
-          label='작품소개'
-          multiline
-          rows={4}
+          value={CurrentTag}
+          name='tag'
+          label='태그'
           variant='outlined'
-          onChange={onDescription}
+          onChange={onCurrentTag}
         />
-        {/* 태그입력창 */}
-        <div>
-          <TextField
-            id='standard-multiline-static'
-            value={CurrentTag}
-            name='tag'
-            label='태그'
-            variant='outlined'
-            onChange={onCurrentTag}
-          />
-          <Button
-            type='button'
-            onClick={onTagClick}
-            variant='contained'
-            color='primary'
+        <Button
+          type='button'
+          onClick={onTagClick}
+          variant='contained'
+          color='primary'
+        >
+          추가
+        </Button>
+        {/* 태그를 추가하면 태그 나타내기 */}
+        {Tags
+          ? Tags.map((tag) => (
+              <div key={tag}>
+                #{tag}
+                <button
+                  type='button'
+                  onClick={(e) => {
+                    e.preventDefault();
+                    const deleteTag = tag;
+                    setTags(Tags.filter((tag) => tag !== deleteTag));
+                  }}
+                >
+                  삭제
+                </button>
+              </div>
+            ))
+          : ""}
+      </div>
+      <Dropzone accept='image/*' onDrop={onDrop}>
+        {({ getRootProps, getInputProps }) => (
+          <section
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "5vh",
+            }}
           >
-            추가
-          </Button>
-          {/* 태그를 추가하면 태그 나타내기 */}
-          {Tags
-            ? Tags.map((tag) => (
-                <div key={tag}>
-                  #{tag}
-                  <button
-                    type='button'
-                    onClick={(e) => {
-                      e.preventDefault();
-                      const deleteTag = tag;
-                      setTags(Tags.filter((tag) => tag !== deleteTag));
-                    }}
-                  >
-                    삭제
-                  </button>
-                </div>
-              ))
-            : ""}
-        </div>
-        <Dropzone accept='image/*' onDrop={onDrop}>
-          {({ getRootProps, getInputProps }) => (
-            <section
+            <div
+              {...getRootProps()}
               style={{
+                width: "40vw",
+                height: "30vh",
+                minWidth: "400px",
+                minHeight: "100px",
+                backgroundColor: "lightgray",
                 display: "flex",
+                flexDirection: "column",
                 justifyContent: "center",
-                marginTop: "5vh",
+                alignItems: "center",
+                border: "dashed 3px gray",
               }}
             >
-              <div
-                {...getRootProps()}
-                style={{
-                  width: "40vw",
-                  height: "30vh",
-                  minWidth: "400px",
-                  minHeight: "100px",
-                  backgroundColor: "lightgray",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  border: "dashed 3px gray",
-                }}
-              >
-                <input {...getInputProps()} />
-                <AddIcon />
-                <p style={{ margin: "3vh 0 3vh 0" }}>
-                  이미지 파일을 선택하거나 끌어다 놓으세요
-                </p>
-                <p style={{ fontSize: "12px", margin: 0 }}>
-                  (파일은 최대 6개까지 업로드 할 수 있습니다.)
-                </p>
-              </div>
-            </section>
-          )}
-        </Dropzone>
-        <div
-          style={{
-            margin: "5vh 0 5vh 0",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "1000px",
-            overflowX: "auto",
-            overflowY: "hidden",
-          }}
-        >
-          {ThumbnailView}
-        </div>
-        <Button variant='contained' color='primary' onClick={OnFileUpload}>
-          올리기
-        </Button>
+              <input {...getInputProps()} />
+              <AddIcon />
+              <p style={{ margin: "3vh 0 3vh 0" }}>
+                이미지 파일을 선택하거나 끌어다 놓으세요
+              </p>
+              <p style={{ fontSize: "12px", margin: 0 }}>
+                (파일은 최대 6개까지 업로드 할 수 있습니다.)
+              </p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+      <div
+        style={{
+          margin: "5vh 0 5vh 0",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "1000px",
+          overflowX: "auto",
+          overflowY: "hidden",
+        }}
+      >
+        {ThumbnailView}
       </div>
-    );
-  };
+      <Button variant='contained' color='primary' onClick={OnFileUpload}>
+        올리기
+      </Button>
+    </div>
+  );
 }
 
 export default withRouter(Newpost);

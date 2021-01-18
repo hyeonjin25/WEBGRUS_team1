fb03f9b69dbd18b23087db0971e20f456951961e
sohyeonjin <shj41738866@gmail.com>
2021-01-18 22:13:38 +0900

    FIx error

--- frontend/src/_actions/favoriteAction.js
+++ frontend/src/_actions/favoriteAction.js
@@ -2,11 +2,10 @@ import axios from "axios";
 import { SERVER_API } from "./config";
 import { POST_FAVORITE, IS_FAVORITE } from "./types";
 
-
 //포스트 좋아요 토글
-export function postFavorite(postid,bool) {
+export function postFavorite({postid, bool}) {
   const request = axios
-    .put(`${SERVER_API}/api/users/favorites/${postid}`,bool)
+    .put(`${SERVER_API}/api/users/favorites/${postid}`, bool)
     .then((res) => res.data);
   return {
     type: POST_FAVORITE,
@@ -14,14 +13,13 @@ export function postFavorite(postid,bool) {
   };
 }
 
-
-  //포스트 좋아요 여부 확인
+//포스트 좋아요 여부 확인
 export function isFavorite(postid) {
-    const request = axios
-      .get(`${SERVER_API}/api/users/favorites/${postid}`)
-      .then((res) => res.data);
-    return {
-      type: IS_FAVORITE,
-      payload: request,
-    };
-  }
+  const request = axios
+    .get(`${SERVER_API}/api/users/favorites/${postid}`)
+    .then((res) => res.data);
+  return {
+    type: IS_FAVORITE,
+    payload: request,
+  };
+}
